361e1421e9566a8f15371da99fef39798465f857
rocky <rb@dusytfeet.com>
2024-07-22 13:51:19 -0400

    Get ready for release 1.1.1

--- admin-tools/make-dist-3.2-3.5.sh
+++ admin-tools/make-dist-3.2-3.5.sh
@@ -22,6 +22,7 @@ source trepanxpy/version.py
 echo $__version__
 
 for pyversion in $PYVERSIONS; do
+    echo --- $pyversion ---
     if ! pyenv local $pyversion ; then
 	exit $?
     fi
@@ -34,3 +35,9 @@ for pyversion in $PYVERSIONS; do
     python setup.py bdist_egg bdist_wheel
     mv -v dist/${PACKAGE}-$__version__-py3-none-any.whl dist/${PACKAGE}-$__version__-py${first_two}-none-any.whl
 done
+
+python ./setup.py sdist
+tarball=dist/${PACKAGE}-${__version__}.tar.gz
+if [[ -f $tarball ]]; then
+    mv -v $tarball dist/${PACKAGE}_32-${__version__}.tar.gz
+fi
