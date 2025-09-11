f0b97456340fe1af81b7e69f4b1a19aa7bdc486c
rocky <rb@dusytfeet.com>
2025-09-11 19:07:55 -0400

    Administrivia

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-PYTHON_VERSION=3.8
+PYTHON_VERSION=3.11
 
-owd=$(pwd)
+trepan_xpy_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
-if [[ $0 == $bs ]] ; then
-    echo "This script should be *sourced* rather than run directly through bash"
-    exit 1
-fi
 mydir=$(dirname $bs)
 fulldir=$(readlink -f $mydir)
 cd $fulldir/..
-(cd ../python-spark && git checkout master && pyenv local $PYTHON_VERSION) && git pull && \
-    (cd ../python-xdis && git checkout master && pyenv local $PYTHON_VERSION) && \
-    (cd ../python3-trepan && git checkout master && pyenv local $PYTHON_VERSION) && git pull && \
-    git checkout master &&  pyenv local $PYTHON_VERSION && git pull
-cd $owd
-rm -v */.python-version || true
+(cd ../python-spark && git checkout master && pyenv local $PYTHON_VERSION && git pull);
+(cd ../python-xdis && git checkout master && pyenv local $PYTHON_VERSION &&U git pull);
+(cd ../../Trepan-Debuggers/python3-trepan && git checkout master && pyenv local $PYTHON_VERSION && git pull)
+git checkout master &&  pyenv local $PYTHON_VERSION && git pull
+cd $trepan_xpy_owd
