6067b8ee84ed33f134a2889219d7a142c5503a10
rocky <rb@dusytfeet.com>
2025-09-11 18:37:55 -0400

    Use pyproject.toml for Python-3.11-3.13

---
+++ admin-tools/setup-python-3.6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PYTHON_VERSION=3.6
+
+trepan_xpy_owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $fulldir/..
+(cd ../python3-trepan && ./admin-tools/setup-python-3.6.sh) && \
+    (cd ../x-python && ./admin-tools/setup-python-3.1.sh)
+git checkout python-3.6-to-3.10 && pyenv local $PYTHON_VERSION && git pull
+cd $trepan_xpy_owd
+rm -v */.python-version || true
