d6ee63f5242d7d97c27f4f09ad2a3e77d8197b19
rocky <rb@dusytfeet.com>
2025-09-16 12:03:01 -0400

    Bang on setup scripts

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -3,11 +3,20 @@ PYTHON_VERSION=3.11
 
 trepan_xpy_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
 mydir=$(dirname $bs)
 fulldir=$(readlink -f $mydir)
-cd $fulldir/..
-(cd ../python-spark && git checkout master && pyenv local $PYTHON_VERSION && git pull);
-(cd ../python-xdis && git checkout master && pyenv local $PYTHON_VERSION &&U git pull);
-(cd ../../Trepan-Debuggers/python3-trepan && git checkout master && pyenv local $PYTHON_VERSION && git pull)
-git checkout master &&  pyenv local $PYTHON_VERSION && git pull
+cd $fulldir/../../../rocky
+pwd
+
+
+(cd ./python-spark && git checkout master && pyenv local $PYTHON_VERSION && git pull)
+(cd ./python-xdis ; git checkout master && pyenv local $PYTHON_VERSION && git pull)
+(cd ../Trepan-Debuggers/python3-trepan && git checkout master && pyenv local $PYTHON_VERSION && git pull)
+(cd $fulldir/.. && git checkout master &&  pyenv local $PYTHON_VERSION && git pull)
 cd $trepan_xpy_owd
+rm -v */.python-version || true
